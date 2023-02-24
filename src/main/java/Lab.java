75e92fc6093306d2b0758f5c503c7dc0d5b9a17d
teriannaj28 <teriannaj@gmail.com>
2023-02-24 19:03:54 +0000

    Coding Challenge: Return Array Contents (Week 2: Day 5)

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,12 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String numString = "";
+        
+        for (int num: nums) {
+            numString += (num + " ");
+        }
+        
+        return numString;
     }
 }
